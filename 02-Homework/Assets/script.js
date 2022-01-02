265236c2cf133892b03b0f0f3ae0ec3892f65d7f
Andrew Tran <andrewtranmsw@gmail.com>
2022-01-01 19:48:08 -0800

    pushing 1_1_22

--- 02-Homework/Assets/script.js
+++ 02-Homework/Assets/script.js
@@ -1,24 +1,23 @@
 // var a = "Hello Poop";
 // console.log(a);
-var time = document.querySelector(".time");
-var secondsLeft = 90;
-
 //This STARTS the game
 let startButton = document.getElementById("start-btn");
 startButton.addEventListener('click', startGame);
-//Functioning
-
+//This grabs the timer element
+var timerEl = document.getElementById("timer-seconds");
+var timeLeft = 900;
 //This grabs the question container which contains the questions and the answers.
 const questionContainerElement = document.getElementById("question-container");
 //This grabs the specific QUESTION element
 let questionElement = document.getElementById('question');
 //this grabs the specific ANSWER element
 const answerButtonsElement = document.getElementById('answer-buttons');
+
 //Questions and answers
 var questionIndex = 0 
 let questionsList = [
     {
-        question: "which is a fruit?",
+        question: "Which is a fruit?",
         answers: [
             { text: 'Bacon', correct: false },
             { text: 'Tomato', correct: true },
@@ -29,28 +28,34 @@ let questionsList = [
     {
         question: "Which is a planet?",
         answers: [
-            { text: 'Venus', correct: false },
-            { text: 'Tomato', correct: true },
-            { text: 'Lettuce', correct: false },
-            { text: 'Cucumber', correct: false }
+            { text: 'Pluto', correct: false },
+            { text: 'Juniper', correct: false },
+            { text: 'Venus', correct: true },
+            { text: 'Omarion', correct: false }
         ]
     },
     {
-        question: "which is a fruit?",
+        question: "which was a US President??",
         answers: [
-            { text: 'Bacon', correct: false },
-            { text: 'Tomato', correct: true },
-            { text: 'Lettuce', correct: false },
-            { text: 'Cucumber', correct: false }
+            { text: 'Alexander Hamilton', correct: false },
+            { text: 'Benjamin Cleveland', correct: false },
+            { text: 'Chester Garfield', correct: false },
+            { text: 'Calvin Coolidge', correct: true }
+        ]
+    },
+    {
+        question: "Which Cherry MX switch is known to be both 'clicky' and tactile?",
+        answers: [
+            { text: 'Red', correct: false },
+            { text: 'Brown', correct: false },
+            { text: 'Blue', correct: true },
+            { text: 'Black', correct: false }
         ]
     },
    
 ]
 
-
-
 //THEN a timer starts and I am presented with a question
-
 
 // function gameStart () {
 //     var timerInterval = setInterval(function() {
@@ -69,15 +74,21 @@ let questionsList = [
 function startGame () {
 console.log('Started');
 startButton.classList.add('hide');
+questionContainerElement.classList.remove('hide');
+showQuestion();
+}
+
+function countdown() {
+
+}
+
+
 // var timerInterval = setInterval(function() {
 //     startGame.addEventListener('click', timerInterval);
 //     secondsLeft--;
 //     time.textContent = secondsLeft + " Seconds Left.";
 // })
-questionContainerElement.classList.remove('hide');
 // setNextQuestion()
-showQuestion()
-}
 
 // function setNextQuestion () {
 //     showQuestion()
@@ -110,25 +121,11 @@ function showQuestion() {
 }
 
 function selectAnswer () {
-
+    
 }
-
-
-
-// var questions = ["Which is a fruit?", "Which is a planet?", "Who was a President of the USA?", "Which Cherry MX switch sounds clicky and feels tactile?"];
-
-// var choices = document.querySelector("#choice");
-// var answersFruit = ["Bacon", "Tomato", "Lettuce", "Cucumber"];
-// var answersPlanet = ["Pluto", "Juniper", "Venus", "Omarion"];
-// var answersPresident = ["Alexander Hamilton", "Benjamin Cleveland", "Chester Garfield", "Calvin Coolidge"];
-// var answersCherry = ["Red", "Brown", "Blue", "Black"];
-
-
-
 
 //Make a function for a timer to be located on the screen that starts ONCE the start button is pressed
 //Make a variable for the 
-
 
 // End-user will need to be presented with a question and 4 multiple choice questions. They must click the correct one, if they do:
 //The timer does not deduct time and the next question with 4 more multiple choice questions is presented
@@ -137,19 +134,8 @@ function selectAnswer () {
  //   - The timer is deducted 10 seconds and...
  // - We move to the next question with 4 more multiple choice questions.
 
-
-
-//WHEN I click the start button
-
-
-
 //WHEN I answer a question
-
-
-
 //THEN I am presented with another question
-
-    
 
 //WHEN I answer a question incorrectly
 //THEN time is subtracted from the clock
